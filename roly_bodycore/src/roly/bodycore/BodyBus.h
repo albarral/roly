7b8802327c005a1ff675d33ce8d9878117a68010
Albarral <albarral@migtron.com>
2018-04-18 13:21:02 +0200

    body core: adapted to new Brooks controls

--- roly_bodycore/src/roly/bodycore/BodyBus.h
+++ roly_bodycore/src/roly/bodycore/BodyBus.h
@@ -9,9 +9,10 @@
 #include <string>
 
 #include "tron/control/brooks/control.h"
-#include "tron/control/brooks/controlT.h"
+#include "tron/control/brooks/control2.h"
+#include "tron/control/brooks/control3.h"
 #include "tron/control/brooks/inhibition.h"
-#include "tron/control/brooks/sensorT.h"
+#include "tron/control/brooks/sensor3.h"
 
 namespace roly
 {
@@ -27,24 +28,24 @@ class BodyBus
         
         // CONTROLS                
         // Artistic module
-        tron::ControlT<int> CO_ARTISTIC_FIGURE;       // id of artistic figure to implement
-        tron::ControlT<float> CO_ARTISTIC_FREQ;       // frequency of cyclic figure (Hz) 
-        tron::ControlT<float> CO_ARTISTIC_SIZE;        // size of cyclic figure (degrees)
-        tron::ControlT<float> CO_ARTISTIC_ORIENTATION;  // orientation of cyclic figure (degrees)
-        tron::ControlT<float> CO_ARTISTIC_RELFACTOR;    // relative xy factor of cyclic figure 
+        tron::Control2 CO_ARTISTIC_FIGURE;       // id of artistic figure to implement
+        tron::Control3 CO_ARTISTIC_FREQ;       // frequency of cyclic figure (Hz) 
+        tron::Control3 CO_ARTISTIC_SIZE;        // size of cyclic figure (degrees)
+        tron::Control3 CO_ARTISTIC_ORIENTATION;  // orientation of cyclic figure (degrees)
+        tron::Control3 CO_ARTISTIC_RELFACTOR;    // relative xy factor of cyclic figure 
         tron::Control CO_ARTISTIC_HALT;                // halt ongoing artistic movement
         
         // Expressive module
-        tron::ControlT<int> CO_EXPRESSIVE_ACTION;   // action to express 
+        tron::Control2 CO_EXPRESSIVE_ACTION;   // action to express 
         tron::Control CO_EXPRESSIVE_HALT;                // halt any ongoing action expression        
                 
         // SENSORS
-        tron::SensorT<float> SI_ARM_PAN;                  // real pan position (degrees)
-        tron::SensorT<float> SI_ARM_TILT;                  // real tilt position (degrees)
-        tron::SensorT<float> SI_ARM_RADIUS;                  // real radial position (cm)
-        tron::SensorT<float> SI_ARM_PANSPEED;         // real pan speed (degrees/s)
-        tron::SensorT<float> SI_ARM_TILTSPEED;         // real tilt speed (degrees/s)
-        tron::SensorT<float> SI_ARM_RADIALSPEED;         // real radial speed (cm/s)
+        tron::Sensor3 SI_ARM_PAN;                  // real pan position (degrees)
+        tron::Sensor3 SI_ARM_TILT;                  // real tilt position (degrees)
+        tron::Sensor3 SI_ARM_RADIUS;                  // real radial position (cm)
+        tron::Sensor3 SI_ARM_PANSPEED;         // real pan speed (degrees/s)
+        tron::Sensor3 SI_ARM_TILTSPEED;         // real tilt speed (degrees/s)
+        tron::Sensor3 SI_ARM_RADIALSPEED;         // real radial speed (cm/s)
         
     public:
         BodyBus();
@@ -60,24 +61,24 @@ class BodyBus
 
         // CONTROLS        
         // Artistic module
-        tron::ControlT<int>& getCO_ARTISTIC_FIGURE() {return CO_ARTISTIC_FIGURE;};
-        tron::ControlT<float>& getCO_ARTISTIC_FREQ() {return CO_ARTISTIC_FREQ;};
-        tron::ControlT<float>& getCO_ARTISTIC_SIZE() {return CO_ARTISTIC_SIZE;};
-        tron::ControlT<float>& getCO_ARTISTIC_ORIENTATION() {return CO_ARTISTIC_ORIENTATION;};
-        tron::ControlT<float>& getCO_ARTISTIC_RELFACTOR() {return CO_ARTISTIC_RELFACTOR;};
+        tron::Control2& getCO_ARTISTIC_FIGURE() {return CO_ARTISTIC_FIGURE;};
+        tron::Control3& getCO_ARTISTIC_FREQ() {return CO_ARTISTIC_FREQ;};
+        tron::Control3& getCO_ARTISTIC_SIZE() {return CO_ARTISTIC_SIZE;};
+        tron::Control3& getCO_ARTISTIC_ORIENTATION() {return CO_ARTISTIC_ORIENTATION;};
+        tron::Control3& getCO_ARTISTIC_RELFACTOR() {return CO_ARTISTIC_RELFACTOR;};
         tron::Control& getCO_ARTISTIC_HALT() {return CO_ARTISTIC_HALT;};
         
         // Expressive module
-        tron::ControlT<int>& getCO_EXPRESSIVE_ACTION() {return CO_EXPRESSIVE_ACTION;};
+        tron::Control2& getCO_EXPRESSIVE_ACTION() {return CO_EXPRESSIVE_ACTION;};
         tron::Control& getCO_EXPRESSIVE_HALT() {return CO_EXPRESSIVE_HALT;};
         
         // SENSORS
-        tron::SensorT<float>& getSI_ARM_PAN() {return SI_ARM_PAN;};        
-        tron::SensorT<float>& getSI_ARM_TILT() {return SI_ARM_TILT;};        
-        tron::SensorT<float>& getSI_ARM_RADIUS() {return SI_ARM_RADIUS;};        
-        tron::SensorT<float>& getSI_ARM_PANSPEED() {return SI_ARM_PANSPEED;};        
-        tron::SensorT<float>& getSI_ARM_TILTSPEED() {return SI_ARM_TILTSPEED;};        
-        tron::SensorT<float>& getSI_ARM_RADIALSPEED() {return SI_ARM_RADIALSPEED;};        
+        tron::Sensor3& getSI_ARM_PAN() {return SI_ARM_PAN;};        
+        tron::Sensor3& getSI_ARM_TILT() {return SI_ARM_TILT;};        
+        tron::Sensor3& getSI_ARM_RADIUS() {return SI_ARM_RADIUS;};        
+        tron::Sensor3& getSI_ARM_PANSPEED() {return SI_ARM_PANSPEED;};        
+        tron::Sensor3& getSI_ARM_TILTSPEED() {return SI_ARM_TILTSPEED;};        
+        tron::Sensor3& getSI_ARM_RADIALSPEED() {return SI_ARM_RADIALSPEED;};        
         
         std::string toString();
 };
